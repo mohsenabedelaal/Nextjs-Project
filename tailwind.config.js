7aa712e6b7663c5f31aefd62717e8cdfcda9ff91
mohsen <mohsen.abedelaal@gmail.com>
2021-09-05 21:13:26 +0300

    The Next JS Project (used tailwind css )

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  mode: "jit", //this is the just in time compilier for tailwind css
+  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      screens: {
+        "3xl": "2000px",
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [require("tailwind-scrollbar-hide")],
+};
