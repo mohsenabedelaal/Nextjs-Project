faf3d10d6e52592337a6d2562da178004c73dad9
mohsen <mohsen.abedelaal@gmail.com>
2021-09-05 21:53:03 +0300

    added the alter for the images.

--- components/Thumbnail.js
+++ components/Thumbnail.js
@@ -17,6 +17,7 @@ const Thumbnail = forwardRef(({ result }, ref) => {
         }
         height={1080}
         width={1920}
+        alt="movie-poster"
       />
       <div className="p-2">
         <p className="truncate max-w-md">{result.overview}</p>
